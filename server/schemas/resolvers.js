3865f200c0300ae4488092f15114ea68dd1a1dce
Corbin Cutter <corbinc03123@gmail.com>
2023-02-19 20:20:26 -0500

    Stuck

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,8 +6,10 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
+                const userData = await User.findOne({})
                 .select('-__v -password')
+                .populate('books')
+            
                 return userData;
             }
             throw new AuthenticationError('You are currently not logged in');
@@ -35,11 +37,11 @@ const resolvers = {
             return { token, user };
         },
         
-        saveBook: async (parent, { book }, context) => {
+        saveBook: async (parent, { Book }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $addToSet: {savedBooks: book} },
+                    { $addToSet: {savedBooks: Book} },
                     { new: true },
                 );
                 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,42 +2,41 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type Query {
-        me: User
+        me: User!
     }
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SavedBookInput): User
-        removeBook(bookId: String!): User
+        addUser(username: String!, email: String!, password: String!): User!
+        saveBook(book: SavedBookInput): User!
+        removeBook(bookId: String!): User!
     }
-
-    input SavedBookInput {
-        authors: [String]
-        title: String
-        description: String
-        bookId: String
-        image: String
-        link: String
-    }
-
     type User {
         _id: ID
-        username: String
-        email: String
-        bookCount: Int
+        username: String!
+        email: String!
+        bookCount: Int!
         savedBooks: [Book]
     }
     type Book {
-        bookId: String
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
+        bookId: String!
+        authors: String!
+        description: String!
+        title: String!
+        image: String!
+        link: String!
+    }
+    input SavedBookInput {
+        authors: String!
+        description: String!
+        bookId: String!
+        image: String!
+        forSale: String!
+        link: String!
+        title: String!
     }
     type Auth {
         token: ID!
-        user: User
+        user: [User]
     }
 `;
 
